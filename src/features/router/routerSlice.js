df8f471412b3fbed34c5b76bb462c3afd13ed4d3
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-12-13 20:52:56 +0100

    :sparkles: Added Router redux feature

---
+++ src/features/router/routerSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Feature functions
+
+/**
+ * Get the page name string reading the window URL
+ * @returns {String} pageName
+ */
+export const getPageNameFromURL = () => {
+    const windowURL = window.location.href;
+    const origin = window.location.origin;
+    const pageName = windowURL.replace(`${origin}/`, '').replace(origin, '');
+    return pageName.length === 0 ? 'Home' : pageName;
+}
+
+/**
+ * Update the new page URL with page name
+ * @param {String} page the new page string 
+ */
+export const updatePageInURL = (page) => {
+    window.history.replaceState(page, '', `${window.location.origin}/${page}`)
+}
+
+// Initial State
+
+const initialState = getPageNameFromURL() || 'Home';
+
+// Slice
+
+export const routerSlice = createSlice({
+    name: 'page',
+    initialState,
+    reducers: {
+        setPage: (state, action) => {
+            const newPage = action.payload;
+            state = newPage;
+            updatePageInURL(newPage);
+        }
+    }
+})
+
+export default routerSlice.reducer;
+
+// Selectors
+
+/**
+ * Select the page from store
+ * @param {*} state the redux state
+ * @returns {String} returns the page name
+ */
+export const selectPage = (state) => state;
