faabfa64c0c2815120650fff2d5c4a630dbb39a3
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-12-15 00:50:46 +0100

    :triangular_flag_on_post: Added language slice to store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import routerReducer from "../features/router/routerSlice";
+import languageReducer from "../features/language/languageSlice";
 
 const store = configureStore({
     reducer: {
-        router: routerReducer
+        router: routerReducer,
+        language: languageReducer
     }
 });
 
