bb04b53771ff30292337dca555582c43378e2e74
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-12-15 00:50:02 +0100

    :sparkles: Added Language Dropdown Component

---
+++ src/components/LanguageDropdown.jsx
@@ -0,0 +1,39 @@
+import { HiOutlineGlobeEuropeAfrica, HiChevronDown } from "react-icons/hi2";
+import { BsCheck} from "react-icons/bs";
+import {useDispatch, useSelector} from "react-redux";
+import {selectLanguage, setLanguage} from "../features/language/languageSlice";
+
+export default function LanguageDropdown() {
+    const dispatch = useDispatch();
+    const language = useSelector(selectLanguage);
+
+    console.log(language)
+
+    return <>
+        <button className="group relative text-sm font-semibold text-indigo-500 border-2 border-indigo-500 rounded-full py-2 px-4 flex items-center transition-all duration-200 ease-in-out hover:text-slate-100 hover:bg-indigo-500 hover:drop-shadow-xl">
+            <div className={"hidden absolute group-focus-within:block bottom-full right-0 bg-white p-3 rounded-lg mb-2 drop-shadow-xl"}>
+                <ul className={"w-full"}>
+                    <li className={"w-full mb-2"}>
+                        <a href="#!" className={"block w-full p-2 px-6 rounded-lg hover:bg-indigo-100 text-slate-800 text-base font-medium flex items-center" + (language === "english" ? " bg-indigo-100" : "")}onClick={() => {
+                            dispatch(setLanguage('english'));
+                        }}>
+                            English {language === "english" ? <span className={"ml-2"}>
+                            <BsCheck size={20} />
+                        </span> : ""}
+                        </a>
+                    </li>
+                    <li className={"w-full"}>
+                        <a href="#!" className={"block w-full p-2 px-6 rounded-lg hover:bg-indigo-100 text-slate-800 text-base font-medium flex items-center" + (language === "french" ? " bg-indigo-100" : "")} onClick={() => {
+                            dispatch(setLanguage('french'));
+                        }}>
+                            Français {language === "french" ? <span className={"ml-2"}>
+                            <BsCheck size={20} />
+                        </span> : ""}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <span className="mr-2"><HiOutlineGlobeEuropeAfrica size={20} /></span> English <span className="ml-2"><HiChevronDown size={20} /></span>
+        </button>
+    </>
+}
