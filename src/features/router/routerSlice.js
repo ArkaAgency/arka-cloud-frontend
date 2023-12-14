fec5e13430c27e48c43dce5ce10f57b6dffbeaa5
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-12-14 12:41:18 +0100

    :zap: Improved redux router

--- src/features/router/routerSlice.js
+++ src/features/router/routerSlice.js
@@ -1,29 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
+import Routes from "../../constants/Routes.constants";
 
 // Feature functions
 
 /**
- * Get the page name string reading the window URL
- * @returns {String} pageName
+ * Get the route object reading the window URL
+ * @returns {Object} Route
  */
-export const getPageNameFromURL = () => {
+export const getRouteFromURL = () => {
     const windowURL = window.location.href;
     const origin = window.location.origin;
-    const pageName = windowURL.replace(`${origin}/`, '').replace(origin, '');
-    return pageName.length === 0 ? 'Home' : pageName;
+
+    const parsedURL = windowURL.replace(`${origin}/`, '').replace(origin, '').replace(/[^a-zA-Z0-9/]/g, '');
+    const routeArgs = parsedURL.split('/');
+    const routeName = routeArgs.shift();
+
+
+    return Routes[routeName] ? {
+        page: routeName,
+        args: routeArgs
+    } : initialState;
 }
 
 /**
  * Update the new page URL with page name
  * @param {String} page the new page string 
  */
-export const updatePageInURL = (page) => {
-    window.history.replaceState(page, '', `${window.location.origin}/${page}`)
+export const updateRouteInURL = (route) => {
+    window.history.replaceState(route.page, '', `${window.location.origin}/${route.page}/${route.args.join('/')}`)
 }
 
 // Initial State
 
-const initialState = getPageNameFromURL() || 'Home';
+const initialState = getRouteFromURL() || {
+    page: 'Home',
+    args: []
+};
 
 // Slice
 
@@ -31,11 +43,11 @@ export const routerSlice = createSlice({
     name: 'page',
     initialState,
     reducers: {
-        setPage: (state, action) => {
-            const newPage = action.payload;
-            state = newPage;
-            updatePageInURL(newPage);
-        }
+        setRoute: (state, action) => {
+            const newRoute = action.payload;
+            state = newRoute;
+            updateRouteInURL(newRoute);
+        },
     }
 })
 
@@ -44,10 +56,28 @@ export default routerSlice.reducer;
 // Selectors
 
 /**
- * Select the page from store
+ * Select the route object from store
+ * @param {*} state the redux state
+ * @returns {String} returns the route object
+ */
+export const selectRoute = (state) => {
+    return state.router;
+}
+
+/**
+ * Select the page name from store
  * @param {*} state the redux state
  * @returns {String} returns the page name
  */
 export const selectPage = (state) => {
-    return state.router
+    return state.router.page
+};
+
+/**
+ * Select the page route args from store
+ * @param {*} state the redux state
+ * @returns {String[]} returns the page route args
+ */
+export const selectRouteArgs = (state) => {
+    return state.router.args
 };
