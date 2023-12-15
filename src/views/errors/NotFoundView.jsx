ab740f8abccda2e6556a22c189673099fbeabc2e
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-12-15 19:39:42 +0100

    :triangular_flag_on_post: Added translations for the not found view

--- src/views/errors/NotFoundView.jsx
+++ src/views/errors/NotFoundView.jsx
@@ -1,6 +1,6 @@
 import text from "../../assets/NotFound_Text.png";
 import illustration from "../../assets/NotFound_Illustration.png";
-
+import t from "../../utils/i18n";
 export default function NotFoundView() {
     return <>
         <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
@@ -9,10 +9,10 @@ export default function NotFoundView() {
                     <div className="absolute">
                         <div className="">
                             <h1 className="my-2 text-gray-800 font-bold text-2xl">
-                                Looks like you've found the doorway to the great nothing
+                                {t`Looks like you've found the doorway to the great nothing`}
                             </h1>
-                            <p className="my-2 text-gray-800">Sorry about that! Please visit our hompage to get where you need to go.</p>
-                            <a href={'/home'} className="block w-fit sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">Take me there!</a>
+                            <p className="my-2 text-gray-800">{t`Sorry about that! Please visit our homepage to get where you need to go.`}</p>
+                            <a href={'/home'} className="block w-fit sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">{t`Take me there!`}</a>
                         </div>
                     </div>
                     <div>
